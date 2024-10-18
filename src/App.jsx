16e332c72c699c4ae505a67eea82dc0b66e94a7a
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-10-18 20:30:37 +0300

    testimonial

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
 import Title from "./components/Title/Title";
 import About from "./components/About/About";
+import Campus from "./components/Campus/Campus";
+import Testimonials from "./components/Testimonials/Testimonials";
+
 
 const App = () => {
   return (
@@ -14,7 +17,10 @@ const App = () => {
         <Title subTitle="what we offer" title="our program" />
         <Programs />
         <About />
-        <Title subTitle="Campus photos" title="gallery" />
+        <Title title="Campus photos" subTitle="gallery" />
+        <Campus />
+        <Title title="What Student Says" subTitle="TESTIMONIALS" />
+        <Testimonials />
       </div>
     </div>
   );
