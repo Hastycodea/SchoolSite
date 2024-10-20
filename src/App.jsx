7d0dc1eb32d4aa01263b22daa2ff5c2928f60af4
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-10-20 16:35:27 +0300

    UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
@@ -8,9 +8,14 @@ import Campus from "./components/Campus/Campus";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 
 
 const App = () => {
+  
+
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar />
@@ -18,7 +23,7 @@ const App = () => {
       <div className="container">
         <Title subTitle="what we offer" title="our program" />
         <Programs />
-        <About />
+        <About setPlayState={setPlayState}/>
         <Title title="Campus photos" subTitle="gallery" />
         <Campus />
         <Title title="What Student Says" subTitle="TESTIMONIALS" />
@@ -27,6 +32,7 @@ const App = () => {
         <Contact />
         <Footer />
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   );
 };
